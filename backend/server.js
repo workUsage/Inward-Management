5e5d38dc232b73ed64b77e681b0e4023b8cd20e8
workUsage <work.usage09@gmail.com>
2024-11-18 23:34:21 +0530

    path change

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
